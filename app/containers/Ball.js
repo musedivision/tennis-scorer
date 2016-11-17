d2dcdf0b873d955f881cc20bb85ba74d6af246b9
musedivison <patrick.mccaffrey.19@gmail.com>
2016-11-17 14:20:07 +1300

    added react-motion spring

--- app/containers/Ball.js
+++ app/containers/Ball.js
@@ -1,14 +1,29 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
+var { Motion, StaggeredMotion ,spring} = require('react-motion')
 
 class Ball extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      open: false
+    }
+  }
 
+  rotate(){
+    this.setState({open: !this.state.open})
+  }
+
+  handleTouchStart(e){
+    e.preventDefault()
+    this.rotate()
+  }
 
   render() {
 
     // styling
     const mixins = {
-      ballRadius: 300
+      ballRadius: 300,
     }
     const ballStyle = {
       height: mixins.ballRadius,
@@ -16,19 +31,20 @@ class Ball extends React.Component {
       borderRadius: '50%',
       backgroundColor: 'yellow',
       boxShadow: '4px 4px',
-      ":hover": {
-        backgroundColor: "red"
-      }
     }
 
     return (
-      <div>
-        <div className="ball-outer" style={ballStyle}>
-          <div>
-
-          </div>
-        </div>
-      </div>
+      <Motion style={{x: spring(this.state.open ? 360 : 0), y:spring(this.state.open ? 10 : 0)}}>
+        {value =>
+          <div onClick={this.handleTouchStart.bind(this)} className="ball-outer" style={{
+              WebkitTransform: `rotate(${value.x}deg)`,
+              transform: `rotate(${value.x}deg)`,
+              boxShadow: `${value.y}px, 4px`,
+            }}>
+            <div style={ballStyle}>
+            </div>
+          </div>}
+      </Motion>
     );
   }
 }
